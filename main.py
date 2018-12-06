fbb832be0d5ec278585572bfcdbefe834f728164
yingtian648@163.com <yingtian648@163.com>
2018-12-06 14:30:16 +0800

    add request totaltime log
    add request totaltime log

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from req_util.api_option import find_case_to_test
 BASE_URL = 'http://172.16.2.74:8080'
 # 默认请求方法
 BASE_METHOD = "GET"
+# 默认请求成功后返回code
+SUCCESS_CODE = 0
 
 # "Content-Type": "application/json"
 BASE_HEADER = {
@@ -16,4 +18,4 @@ BASE_HEADER = {
 }
 
 if __name__ == '__main__':
-    find_case_to_test(BASE_URL,BASE_HEADER,BASE_METHOD)
+    find_case_to_test(BASE_URL, BASE_HEADER, BASE_METHOD)
--- req_util/base_request.py
+++ req_util/base_request.py
@@ -5,11 +5,8 @@
 # Desc :
 from urllib.request import Request, urlopen
 from urllib.parse import quote, urlencode
-import json
+import json, time
 from req_util.log_util import log
-
-success_code = 0  # 请求成功的返回code
-
 
 def post_api(url, params: dict, header: dict, post_body: dict):
     """
@@ -60,6 +57,7 @@ def get_api(url, params: dict, header: dict):
 
 
 def process_result(url, req, params, header, methodStr):
+    from main import SUCCESS_CODE  # 请求成功的返回code
     """
     处理请求返回结果
     :param req:
@@ -67,21 +65,23 @@ def process_result(url, req, params, header, methodStr):
     """
     result_content = None
     try:
+        t1 = time.time()
         result_content = urlopen(req).read().decode('utf-8')
+        tottime = ("use_tottime: " + str(time.time() - t1)) + " ms"
         result_content = json.loads(result_content)
-        if result_content['code'] and not result_content['code'] == success_code:
-            log('------操作失败------')
+        if result_content['code'] and not result_content['code'] == SUCCESS_CODE:
+            log('------ Failure ------')
             log('request_url:' + url)
             log('request_method:' + methodStr)
             log('request_header:' + str(header))
             log('request_params:' + str(params))
             log('response:' + str(result_content))
         else:
-            log('------操作成功------')
+            log('------ Success ' + tottime + ' ------')
             log(str(result_content))
 
     except Exception as excp:
-        log('------请求异常------')
+        log('------ Exception ------')
         log('request_url:' + url)
         log('request_method:' + methodStr)
         log('request_header:' + str(header))
